0a1ca87cd7fb62c2c218ee87798ddfa1ae7e4a93
Samuel Wu <cernunnos1710@gmail.com>
2023-08-26 07:49:39 +0800

    Initial commit

---
+++ lib/gene_evolve.ex
@@ -0,0 +1,18 @@
+defmodule GeneEvolve do
+  @moduledoc """
+  Documentation for `GeneEvolve`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GeneEvolve.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gene_evolve_test.exs
@@ -0,0 +1,8 @@
+defmodule GeneEvolveTest do
+  use ExUnit.Case
+  doctest GeneEvolve
+
+  test "greets the world" do
+    assert GeneEvolve.hello() == :world
+  end
+end
